afde30ff2cccc4abc7d9b85935693dd4dde34526
oki2a24 <oki2a24@oki2a24.com>
2017-08-04 06:12:26 +0900

    別の環境で動くことを確認。Macで動かぬ

--- centos-7-x86_64/scripts.sh
+++ centos-7-x86_64/scripts.sh
@@ -21,8 +21,10 @@ sudo sed -i -e 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
 sudo yum -y install epel-release
 sudo sed -i -e 's/^enabled=1/enabled=0/' /etc/yum.repos.d/epel.repo
 # 必要パッケージのインストール
-sudo yum -y install bzip2 gcc make perl
+sudo yum -y install bzip2 gcc kernel-devel make perl
 sudo yum -y --enablerepo=epel install dkms
+sudo systemctl start dkms
+sudo systemctl enable dkms
 # VirtualBox Guest Additions のインストール
 sudo mount -o loop,ro ~/VBoxGuestAdditions.iso /mnt/
 sudo /mnt/VBoxLinuxAdditions.run || :
